8f014a85db0caeeef28f62c32ddbabaec1a47090
brandon.dev1318 <mendezrodrigo160@gmail.com>
2025-12-03 18:47:28 -0600

    borrado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  url_api: 'https://control-escolar-buap-api.onrender.com'
+  production: true
+  //url_api: 'https://control-escolar-buap-api.onrender.com'
 };
